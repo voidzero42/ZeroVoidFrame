820592eedaee32f015991d2bcac42f0834724cff
wugefei <695943001@qq.com>
2016-04-14 19:36:28 +0800

    测试开发AppWidget

---
+++ app/src/main/java/cc/zerovoid/test/NoteWidget.java
@@ -0,0 +1,14 @@
+package cc.zerovoid.test;
+
+import android.appwidget.AppWidgetHost;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2016/4/14.
+ */
+public class NoteWidget extends AppWidgetHost{
+
+    public NoteWidget(Context context, int hostId) {
+        super(context, hostId);
+    }
+}
